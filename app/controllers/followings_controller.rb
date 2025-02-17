0f0656275e1cb6b31d8df7021c06721646398547
marvin <marvin.mitchell@amarbank.co.id>
2025-02-17 13:52:49 +0700

    feat(follow): add controller implementation to retrieve sleep records of all followed users

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -2,5 +2,19 @@ class FollowingsController < ApplicationController
   before_action :authenticate_user!
 
   def sleep_records
+    pagy, sleep_records = pagy(
+      SleepRecord.joins(user: :followers)
+        .where(users: { id: current_user.followings.pluck(:id) })
+        .where("clock_in >= ?", 1.week.ago)
+        .order(duration: :desc)
+    )
+
+    render json: { data: sleep_records,
+      pagination: {
+        total: pagy.count,
+        current_page: pagy.page,
+        limit_per_page: pagy.limit
+      }
+    }
   end
 end
